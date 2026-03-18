90b6ffde532bd1abac00de7f8827b4f8f06c1cf1
Lzk26 <luiz.candido@aluno.ifsp.edu.br>
2026-03-18 13:53:08 -0300

    Atividade do Melo

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,28 @@
+import { 
+    Controller,
+    Get,
+    Query,
+    Param 
+} from '@nestjs/common';
+import { TasksService } from './tasks.service'
+
+@Controller('tasks')
+export class TasksController {
+    constructor(private readonly taskService: TasksService) { }
+
+    @Get()
+    getTasks() {
+        return this.taskService.listAllTasks()
+    }
+
+    @Get("/busca") 
+    findManyTasks(@Query() queryParam: any) {
+        return this.taskService
+    }
+    
+
+    @Get(":id")
+    findSingleTask(@Param('id') id: string) {
+        return this.taskService.findOneTask(id)
+    }
+}
